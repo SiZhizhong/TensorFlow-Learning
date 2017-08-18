dd107a93f8ce0e9821514a4f54e4bcb8e1d94fa9
SiZhizhong <31054824+SiZhizhong@users.noreply.github.com>
2017-08-19 00:16:46 +0800

    Note5

---
+++ Notes/Note5.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+#config for Model
+class config:
+    def __init__(self,training_rate=0.1):
+        self.training_rate=training_rate
+
+
+class MnistModel:
+    def __init__(self):
+        raise NotImplementedError
+
+    def read_in_data(self):
+        raise NotImplementedError
+
+    def create_placeholder(self):
+        raise  NotImplementedError
+
+    def create_traning_variable(self):
+        raise  NotImplementedError
+
+    
