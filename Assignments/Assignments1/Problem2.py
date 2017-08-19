4b8accc3558de37cb4be9440d745aa51cbf63d62
SiZhizhong <31054824+SiZhizhong@users.noreply.github.com>
2017-08-19 23:25:04 +0800

    problem2

--- Assignments/Assignments1/Problem2.py
+++ Assignments/Assignments1/Problem2.py
@@ -9,8 +9,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-
+"""
 # Add A hidden layer
 def ImprovedMNIST():
     # constant
@@ -89,11 +90,96 @@ def ImprovedMNIST():
 
         print("Accuracy:{0}".format(total_correct / data.train.num_examples))
 
-import  xlrd
+"""
+
+
 def heart_disease_predict():
-    book=xlrd.open_workbook()
+    # step 1:read in data and format it
+    file = open("./data/heart_disease/heart.csv")
+    csvreader = csv.reader(file)
+    X = []
+    Y = []
+    i = 0
+    next(csvreader)
+    for row in csvreader:
+        X.append([])
+        Y.append([float(row[9])])
+        for j in range(4):
+            X[i].append(float(row[j]))
+
+        if row[4] == "Present":
+            X[i].append(1.0)
+        else:
+            X[i].append(0.0)
+
+        for j in range(5, 9):
+            X[i].append(float(row[j]))
+        i += 1
+
+    train_size = 300
+    feature_size = len(X[0])
+    batch_size = 10
+    hidden_layer = 5
+
+    epoch = 200
+
+    X_train = X[0:train_size]
+    Y_train = Y[0:train_size]
+
+    X_test = X[train_size:len(X)]
+    Y_test = Y[train_size:len(Y)]
+
+    training_rate = 0.0002
+    """
+    Build Model
+    """
+    # step 2: Build placeholder
+    X = tf.placeholder(dtype=tf.float32, name="X")
+    Y = tf.placeholder(dtype=tf.float32, name="Y")
+
+    # step 3:Build variable
+    w1 = tf.Variable(tf.zeros(shape=[feature_size, hidden_layer]), trainable=True, name="weight")
+    b1 = tf.Variable(tf.zeros(shape=[1, hidden_layer]), trainable=True, name="bias")
+
+    w2 = tf.Variable(tf.zeros(shape=[hidden_layer, 1]), trainable=True, name="weight")
+    b2 = tf.Variable(tf.zeros(shape=[1, 1]), trainable=True, name="bias")
+
+    # step 4:Build predict function
+    sig = tf.matmul(X, w1) + b1
+    hidden = tf.sigmoid(sig)
+    sig2 = tf.matmul(hidden, w2) + b2
+    Y_predict=tf.sigmoid(sig2)
+
+    loss = tf.reduce_mean((1-Y)*tf.abs(tf.log(1-Y_predict))+tf.abs(Y*tf.log(Y_predict)))
+
+    # step 5: Build optimizer
+    optimizer = tf.train.GradientDescentOptimizer(training_rate).minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        writer = tf.summary.FileWriter("./graphs/Problem3", graph=sess.graph)
+
+        n_batches = train_size / batch_size
+        for i in range(epoch):
+            total_loss = 0
+
+            for j in range(int(n_batches)):
+                start_num = j * batch_size
+                _, loss_ = sess.run([optimizer, loss], feed_dict={X: X_train[start_num:start_num + batch_size],
+                                                                  Y: Y_train[start_num:start_num + batch_size]})
+                total_loss += loss_
+
+
+            print("Epoch{0}:{1}".format(i, total_loss/n_batches))
+            writer.close()
+
+        Y_test_predict = sess.run(Y_predict, feed_dict={X: X_test})
+        correct=np.abs(np.around(Y_test_predict)-Y_test)
+        accuracy=(len(Y_test)-np.sum(correct))/len(Y_test)
+        print(accuracy)
+
 
 
 if __name__ == "__main__":
-    #ImprovedMNIST()
+    # ImprovedMNIST()
     heart_disease_predict()
