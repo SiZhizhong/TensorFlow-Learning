d62394017ae8b072a27629c0d3df46a226ba60b6
SiZhizhong <31054824+SiZhizhong@users.noreply.github.com>
2017-08-20 21:08:14 +0800

    lesson5

--- MyCodes/Lesson5.py
+++ MyCodes/Lesson5.py
@@ -1,21 +1,58 @@
+# Rewrite the MNIST model by object orient
+
+from TFModel import TFModel
 import tensorflow as tf
-
-#config for Model
-class config:
-    def __init__(self,training_rate=0.1):
-        self.training_rate=training_rate
+from tensorflow.examples.tutorials.mnist import input_data
 
 
-class MnistModel:
-    def __init__(self):
-        raise NotImplementedError
+class MNISTModel(TFModel):
+    def load_data(self):
+        self.all_data = input_data.read_data_sets('./data/mnist', one_hot=True)
 
-    def read_in_data(self):
-        raise NotImplementedError
+    def add_placeholders(self):
+        # for input data
+        self.X = tf.placeholder(dtype=tf.float32, shape=[None, 784], name="X")
+        # for input label
+        self.Y = tf.placeholder(dtype=tf.float32, shape=[None, 10], name="Y")
 
-    def create_placeholder(self):
-        raise  NotImplementedError
+    def add_model(self, input_data=None):
+        self.hidden_layer_size = 100
+        self.weight1 = tf.Variable(tf.random_normal(shape=[784, self.hidden_layer_size]), trainable=True,
+                                   name="weight1")
+        self.bias1 = tf.Variable(tf.random_normal(shape=[1, self.hidden_layer_size]), trainable=True, name="bias1")
 
-    def create_traning_variable(self):
-        raise  NotImplementedError
+        self.weight2 = tf.Variable(tf.random_normal(shape=[self.hidden_layer_size, 10]), trainable=True, name="weight2")
+        self.bias2 = tf.Variable(tf.random_normal(shape=[1, 10]), trainable=True, name="bias2")
 
+        self.hidden_layer_logits = tf.matmul(self.X, self.weight1) + self.bias1
+        self.hidden_layer_output = tf.sigmoid(self.hidden_layer_logits)
+
+        self.out_layer_logitst = tf.matmul(self.hidden_layer_output, self.weight2) + self.bias2
+        return self.out_layer_logitst
+
+    def add_loss_op(self, pred):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=pre, logits=self.out_layer_logitst)
+        self.loss_op = tf.reduce_mean(cross_entropy)
+        return self.loss_op
+
+    def create_feed_dict(self, input_batch, label_batch=None):
+        if label_batch == None:
+            feed_dict = {self.X: input_data}
+        else:
+            feed_dict = {self.X: input_data, self.Y: label_batch}
+        return feed_dict
+
+    def add_training_op(self, loss_op):
+        self.optimizer = tf.train.GradientDescentOptimizer(self.config.training_rate).minimize(loss_op)
+
+    def run_epoch(self, sess, input_data, input_labels):
+        feed_dict = self.create_feed_dict(input_data, input_labels)
+        sess = tf.Session()
+        sess.run([])
+
+    def __init__(self, Config):
+        self.config = Config
+
+
+class Config(object):
+    training_rate = 0.1
