2f9cb841244a94ee790cff93af7ccb897bd55e89
SiZhizhong <31054824+SiZhizhong@users.noreply.github.com>
2017-08-27 22:22:21 +0800

    lesson 5

--- MyCodes/Lesson5.py
+++ MyCodes/Lesson5.py
@@ -50,35 +50,42 @@ class MNISTModel(TFModel):
                                                                                                global_step=self.global_step)
 
     def run_epoch(self, sess, input_data=None, input_labels=None):
+        with tf.name_scope("summaries"):
+            tf.summary.scalar("loss", self.loss_op)
+            tf.summary.histogram("loss_histogram", self.loss_op)
+            self.all_summary = tf.summary.merge_all()
+
         n_batch = int(self.all_data.train.num_examples / self.config.batch_size)
         total_loss = 0
         for _ in range(n_batch):
             x_batch, y_batch = self.all_data.train.next_batch(self.config.batch_size)
             feed_dict = self.create_feed_dict(x_batch, y_batch)
-            _, loss = sess.run([self.optimizer, self.loss_op], feed_dict=feed_dict)
+            _, loss, summary = sess.run([self.optimizer, self.loss_op, self.all_summary], feed_dict=feed_dict)
             total_loss += loss
         average_loss = total_loss / n_batch
-        return average_loss
+        return (average_loss, summary)
 
     def fit(self, sess, input_data=None, input_labels=None):
 
         saver = tf.train.Saver(max_to_keep=5)
 
-        #sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
         self.writer = tf.summary.FileWriter('./graphs/mnst', graph=sess.graph)
         loss_array = []
         for epoch in range(self.config.epochs):
-            epoch_loss = self.run_epoch(sess, input_data, input_labels)
+            epoch_loss, summary = self.run_epoch(sess, input_data, input_labels)
+
             loss_array.append([epoch, epoch_loss])
             saver.save(sess, './checkpoint/MNIST', global_step=self.global_step)
-
+            self.writer.add_summary(summary)
+        self.writer.close()
         return loss_array
 
     def predict(self, sess, input_data, input_labels=None):
         # read in trained model variables,return the number
-        logits=sess.run(self.out_layer_logitst,feed_dict={self.X:input_data})
-        num=sess.run(tf.nn.softmax(logits))
-        predict_num=numpy.argmax(num)
+        logits = sess.run(self.out_layer_logitst, feed_dict={self.X: input_data})
+        num = sess.run(tf.nn.softmax(logits))
+        predict_num = numpy.argmax(num)
         return predict_num
         # if not trained ,return None
 
@@ -88,7 +95,7 @@ class MNISTModel(TFModel):
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
 
-    def caculate_accuracy(self,sess):
+    def caculate_accuracy(self, sess):
         Y_predict = tf.nn.softmax(logits=self.out_layer_logitst)
         correct = tf.equal(tf.arg_max(Y_predict, 1), tf.arg_max(self.Y, 1))
         accuracy = tf.reduce_sum(tf.cast(correct, tf.float32))
@@ -96,12 +103,12 @@ class MNISTModel(TFModel):
         total_correct = 0
         for _ in range(n_batches):
             x_batches, y_batches = self.all_data.train.next_batch(self.config.batch_size)
-            accuracy_num = sess.run(accuracy, feed_dict={self.X: x_batches,self. Y: y_batches})
+            accuracy_num = sess.run(accuracy, feed_dict={self.X: x_batches, self.Y: y_batches})
             total_correct += accuracy_num
 
-        print("Accuracy:{0}".format(total_correct /self.all_data.train.num_examples ))
+        print("Accuracy:{0}".format(total_correct / self.all_data.train.num_examples))
 
-    def __init__(self, Config,sess):
+    def __init__(self, Config, sess):
         # constant settings
         self.config = Config
         # read in data
@@ -116,7 +123,6 @@ class MNISTModel(TFModel):
         sess.run(tf.global_variables_initializer())
 
 
-
 class Config(object):
     training_rate = 0.1
     epochs = 30
@@ -125,29 +131,24 @@ class Config(object):
 
 if __name__ == "__main__":
     sess = tf.Session()
-    my_mnist_model = MNISTModel(Config(),sess)
+    my_mnist_model = MNISTModel(Config(), sess)
     my_mnist_model.load_data()
 
-
     my_mnist_model.restore(sess)
-    #loss_array=my_mnist_model.fit(sess)
+    # loss_array = my_mnist_model.fit(sess)
     #print(loss_array)
-    #my_mnist_model.caculate_accuracy(sess)
+    # my_mnist_model.caculate_accuracy(sess)
 
 
 
 
-    x_batch,y_batch=my_mnist_model.all_data.train.next_batch(1000)
+    x_batch, y_batch = my_mnist_model.all_data.train.next_batch(1000)
     for i in range(5):
-        number=numpy.random.randint(0,1000-1)
+        number = numpy.random.randint(0, 1000 - 1)
 
-        data=numpy.reshape(x_batch[number],[1,784])
-        predict_num=my_mnist_model.predict(sess,data)
-        print("predict_num {0},actual_num {1}".format(predict_num,numpy.argmax(y_batch[number])))
+        data = numpy.reshape(x_batch[number], [1, 784])
+        predict_num = my_mnist_model.predict(sess, data)
+        print("predict_num {0},actual_num {1}".format(predict_num, numpy.argmax(y_batch[number])))
 
-        plt.imshow(numpy.reshape(data,[28,28]))
+        plt.imshow(numpy.reshape(data, [28, 28]))
         plt.show()
-
-
-
-
