8a10119b25873d66ec7de8af988486b1cd381bc8
SiZhizhong <31054824+SiZhizhong@users.noreply.github.com>
2018-04-17 20:26:28 +0800

    None

--- MyCodes/linear_regression_2.py
+++ MyCodes/linear_regression_2.py
@@ -1,6 +1,8 @@
 import  tensorflow as  tf
 import  numpy as  np
 import csv
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 
@@ -11,31 +13,35 @@ class LinearModel:
         self.input_feature_size=input_feature_size
         self.output_feature_size=output_feature_size
         self.trained=False
+        self.cofigure_model()
 
-
-    def add_placeholder(self):
-        self.X=tf.placeholder(tf.float32,[self.input_feature_size,None],name="X")
-        self.Y=tf.placeholder(tf.float32,[self.output_feature_size,None],name="Y")
-
-    def add_variable(self):
-        self.W=tf.get_variable("Weight",[self.output_feature_size,self.input_feature_size],tf.float32,
-                               initializer=tf.random_normal_initializer(0,0.1))
-        self.bias=tf.get_variable("bias",[1,self.output_feature_size],tf.float32,
-                                  initializer=tf.zeros_initializer)
 
     def cofigure_model(self):
-        self.prediction=tf.matmul(self.W,self.X)+self.bias
+        self.X = tf.placeholder(tf.float32, [self.input_feature_size, None], name="X")
+        self.Y = tf.placeholder(tf.float32, [self.output_feature_size, None], name="Y")
+
+        self.W = tf.get_variable("Weight", [int(self.input_feature_size/2), self.input_feature_size], tf.float32,
+                                 initializer=tf.random_normal_initializer(0, 0.1))
+        self.bias = tf.get_variable("bias", [int(self.input_feature_size/2),1], tf.float32,
+                                    initializer=tf.zeros_initializer)
+        self.logits=tf.matmul(self.W,self.X)+self.bias
+        self.activation=tf.nn.tanh(self.logits)
+
+        self.W1=tf.get_variable("Weight2", [self.output_feature_size, int(self.input_feature_size/2)], tf.float32,
+                                 initializer=tf.random_normal_initializer(0, 0.1))
+        self.bias1 = tf.get_variable("bias2", [self.output_feature_size,1], tf.float32,
+                                    initializer=tf.zeros_initializer)
+        self.output_sigmoid=tf.matmul(self.W1,self.activation)+self.bias1
+        self.prediction=tf.nn.sigmoid(self.output_sigmoid)
         self.loss=tf.reduce_mean(tf.square(self.prediction-self.Y))
 
 
 
     def train(self,print_eopoch=False):
-        self.add_placeholder()
-        self.add_variable()
-        self.cofigure_model()
-        self.optimizer=tf.train.AdadeltaOptimizer(0.001).minimize(self.loss)
+
+        self.optimizer=tf.train.AdadeltaOptimizer(0.0001).minimize(self.loss)
         self.saver=tf.train.Saver(max_to_keep=5)
-        for i in range(1000):
+        for i in range(1001):
             with tf.Session() as sess:
                 sess.writer=tf.summary.FileWriter("./graphs",sess.graph)
                 sess.run(tf.global_variables_initializer())
@@ -43,6 +49,8 @@ class LinearModel:
                 if print_eopoch and i %50==0:
                     print("Epoch ",i,": ","loss: ",l)
                     self.saver.save(sess,"./checkpoint/LinearModel")
+        self.trained=True
+
 
     def load_data(self,path):
         file=open(path,'r')
